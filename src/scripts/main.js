318a8c19674651e07b41c42fbbe7b4922cf05d61
Roman Shevchyshyn <romanshevchyshyn1998@gmail.com>
2022-07-03 23:03:54 +0300

    added sort-by custom

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const select = function() {
+  const selectHeader = document.querySelectorAll('.select__by-header');
+  const selectItem = document.querySelectorAll('.select__by-body-item');
+
+  selectHeader.forEach(item => {
+    item.addEventListener('click', selectToggle);
+  });
+
+  selectItem.forEach(item => {
+    item.addEventListener('click', selectChoose);
+  });
+
+  function selectToggle() {
+    this.parentElement.classList.toggle('select__by--isActive');
+  }
+
+  function selectChoose() {
+    const text = this.innerText;
+    const selectBy = this.closest('.select__by');
+    const currentText = selectBy.querySelector('.select__by-current');
+
+    currentText.innerText = text;
+    selectBy.classList.remove('select__by--isActive');
+  }
+};
+
+select();
